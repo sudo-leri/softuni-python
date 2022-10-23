e1a5f1b3985a3fa03fa60506afc86af6f9afe49a
Valeri Terziyski <valeri.terziyski@mentormate.com>
2022-10-23 12:06:35 +0300

    Add break

--- 1.py
+++ 1.py
@@ -21,5 +21,6 @@ for day in range(1, days + 1):
 
     if energy <= 0:
         print(f"You will run out of energy. You will be left with {food:.2f} food and {water:.2f} water.")
+        break
     elif day == days:
         print(f"You are ready for the quest. You will be left with - {energy:.2f} energy!")
